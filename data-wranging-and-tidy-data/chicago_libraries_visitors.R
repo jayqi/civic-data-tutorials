6edb342ee35a76590b9ec57ee3b33cb9d3045862
Jay Qi <jay.qi@uptake.com>
2017-03-28 19:39:35 -0500

    Updated chicago_libraries_visitors.R from session

--- data-wranging-and-tidy-data/chicago_libraries_visitors.R
+++ data-wranging-and-tidy-data/chicago_libraries_visitors.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 
 # install.packages('RSocrata')
 
@@ -12,12 +13,16 @@ read.socrata.DT <- function(url) {
     RSocrata::read.socrata(url)
     )
 }
+# data.table
+# dplyr
 
 visitors2016DT <- read.socrata.DT("https://data.cityofchicago.org/dataset/Libraries-2016-Visitors-by-Location/cpc6-pxmp")
 # https://data.cityofchicago.org/dataset/Libraries-2016-Visitors-by-Location/cpc6-pxmp
 
+head(visitors2016DT)
 str(visitors2016DT)
 summary(visitors2016DT)
+tail(visitors2016DT)
 
 # Some basic cleaning before we begin
 # - Last row is blank in the dataset, got read in as all NA
@@ -27,6 +32,9 @@ BasicCleaning <- function(DT) {
   DT[, LOCATION := trimws(LOCATION)]
   return(DT)
 }
+visitors2016DT[LOCATION == 'West Town']
+visitors2016DT[LOCATION == 'Whitney M. Young, Jr.']
+visitors2016DT[LOCATION == 'Whitney M. Young, Jr. ']
 visitors2016DT <- BasicCleaning(visitors2016DT)
 
 
@@ -48,6 +56,12 @@ visitors2016DT[LOCATION == "Lincoln Park", YTD]
 # - What if we want to look at changes for both 2015 and 2016? 
 #   How do I intelligently append the 2015 dataset?
 
+monthNames <- names(visitors2016DT)[2:13]
+
+# Tidy data
+
+# https://cran.r-project.org/web/packages/data.table/vignettes/datatable-reshape.html
+
 # Get rid of the YTD column. It's easy enough to recalculate if needed.
 TidyingUp <- function(DT) {
   DT[, YTD := NULL]
@@ -65,14 +79,23 @@ TidyingUp <- function(DT) {
 }
 visitors2016DT <- TidyingUp(visitors2016DT)
 
+visitors2016DT[MONTH == "MARCH", mean(VISITORS)]
+
+visitors2016DT[LOCATION == "Lincoln Park", sum(VISITORS)]
+
+visitors2016DT[, sum(VISITORS), by = 'LOCATION']
+visitors2016DT[, sum(VISITORS), by = 'MONTH']
+
 # Questions that are more annoying to answer in this format:
 # - What are the peak months for each library?
 visitors2016DT[, max(VISITORS), by = 'LOCATION']
 visitors2016DT[BUILDING.TOTAL == TRUE, max(VISITORS), by = 'LOCATION']
 visitors2016DT[BUILDING.TOTAL == TRUE, max(VISITORS), by = 'LOCATION'][order(-V1)][1:5]
 
-visitors2016DT[, .I[which.max(VISITORS)], by = 'LOCATION']
-visitors2016DT[BUILDING.TOTAL == TRUE, .I[which.max(VISITORS)], by = 'LOCATION']
+peakMonthRowNos <- visitors2016DT[, .I[which.max(VISITORS)], by = 'LOCATION'][, V1]
+visitors2016DT[peakMonthRowNos]
+
+visitors2016DT[peakMonthRowNos][BUILDING.TOTAL == TRUE]
 
 # - How many visitors were there for each library in Q1 of 2016? 
 #     - What about in total? 
@@ -90,16 +113,21 @@ MonthAndYearToDateTime <- function(month, year) {
     )
   return(lubridate::ymd(dateTimeString))
 }
-CreateDateTimeColumn <- function(DT) {
+CreateDateTimeColumn <- function(DT, year) {
+  
   DT[, numMonth := match(MONTH, monthNames)]
-  DT[, numYear := 2016]
+  DT[, numYear := year]
   DT[, dateTime := MonthAndYearToDateTime(numMonth, numYear)]
 }
-visitors2016DT <- CreateDateTimeColumn(visitors2016DT)
+visitors2016DT <- CreateDateTimeColumn(visitors2016DT, 2016)
 
-(ggplot(data = visitors2016DT
-        , mapping = aes(x = dateTime, y = VISITORS, color = LOCATION)
-        )
+(ggplot(data = visitors2016DT, mapping = aes(x = dateTime, y = VISITORS, color = LOCATION))
++ geom_line()
+)
+
+top5 <- visitors2016DT[, sum(VISITORS), by = 'LOCATION'][order(-V1)][1:5][, LOCATION]
+
+(ggplot(data = visitors2016DT[LOCATION %in% top5], mapping = aes(x = dateTime, y = VISITORS, color = LOCATION))
 + geom_line()
 )
 
@@ -108,13 +136,15 @@ visitors2016DT <- CreateDateTimeColumn(visitors2016DT)
 
 visitors2015DT <- read.socrata.DT("https://data.cityofchicago.org/dataset/Libraries-2016-Visitors-by-Location/cpc6-pxmp")
 # https://data.cityofchicago.org/dataset/Libraries-2015-Visitors-by-Location/7imc-umy4
+# visitors2015DT <- BasicCleaning(visitors2015DT)
+# visitors2015DT <- CreateDateTimeColumn(TidyingUp(BasicCleaning(visitors2015DT)))
+
 visitors2015DT <- (visitors2015DT
                    %>% BasicCleaning
                    %>% TidyingUp
-                   %>% CreateDateTimeColumn
+                   %>% CreateDateTimeColumn(year = 2015)
 )
 
 visitorsDT <- data.table::rbindlist(list(visitors2015DT, visitors2016DT))
 
 
-# https://cran.r-project.org/web/packages/data.table/vignettes/datatable-reshape.html
